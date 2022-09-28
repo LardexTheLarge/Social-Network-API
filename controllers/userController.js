f29127f274a7e4671bd38b4a00e30b7328b87a4d
Gabe Vee <0gabevee0@gmail.com>
2022-09-27 22:14:27 -0600

    added get and get by id for user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,22 @@
 const { User, Thought } = require("../models");
 
 //TODO: add route controller
-module.exports = {};
+module.exports = {
+  //GET all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  //GET user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.UserId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
